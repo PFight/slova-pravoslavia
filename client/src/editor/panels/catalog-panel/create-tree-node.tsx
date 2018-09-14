9ba12ffb162d42ced402ceb02c25eaf320103b5f
PFight <PFight77@gmail.com>
2018-09-14 21:05:16 +0300

    Changed catalog icons.

--- client/src/editor/panels/catalog-panel/create-tree-node.tsx
+++ client/src/editor/panels/catalog-panel/create-tree-node.tsx
@@ -1,12 +1,15 @@
 import { CatalogNode } from '@common/models/CatalogNode';
-import { ITreeNode } from "@blueprintjs/core";
+import { ITreeNode, Icon } from "@blueprintjs/core";
+import React from 'react';
 
 export function createTreeNode(catalogNode: CatalogNode, parentNode?: ITreeNode<CatalogNode>): ITreeNode<CatalogNode> {
   let node: ITreeNode<CatalogNode> = {
     id: catalogNode.id,
     label: catalogNode.data && catalogNode.data.caption || '',
     nodeData: catalogNode,
-    icon: catalogNode.children && catalogNode.children.length > 0 ? "folder-close" : "layout-linear"
+    icon: catalogNode.children && catalogNode.children.length > 0 ? 
+      <Icon icon="duplicate" className="bp3-tree-node-icon" iconSize={12} /> : 
+      <Icon icon="square" className="bp3-tree-node-icon" iconSize={10} style={{marginTop: "4px"}} />
   };
   if (parentNode) {
     parentNode.childNodes = parentNode.childNodes || [];
